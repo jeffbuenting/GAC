c0f676baaa46511f1d45f62669a13b11f1c852f5
jeffbuenting <jeffbuenting@gmail.com>
2016-05-11 15:55:44 -0400

    initial upload

---
+++ Reinstall-GACAssemblies.ps1
@@ -0,0 +1,14 @@
+﻿get-childitem 'C:\Program Files\Microsoft Dynamics CRM\Server\bin\assembly\*.dll' -Exclude 'EntityFramework*' | foreach {
+    $Assembly = $_
+    if ( Get-ChildItem C:\Windows\Microsoft.NET\assembly\GAC_MSIL | where name -eq $Assembly.BaseName ) {
+            Write-Output "Uninstall / reinstall $($Assembly.Name)"
+            & 'C:\Releases\NETFX 4.0 Tools\gacutil.exe' /u $Assembly.Name
+            & 'C:\Releases\NETFX 4.0 Tools\gacutil.exe' /i $Assembly.FullName
+           #Start-Process -FilePath 'C:\Releases\NETFX 4.0 Tools\gacutil.exe' -ArgumentList "/u $($_.Name)" -Wait
+            #Start-Process -FilePath 'C:\Releases\NETFX 4.0 Tools\gacutil.exe' -ArgumentList "/i $($_.FullName)" -Wait
+        }
+        else {
+            Write-Output "Install $($Assembly.Name)"
+            & 'C:\Releases\NETFX 4.0 Tools\gacutil.exe' /i $Assembly.FullName
+    }
+}
